459d3ac4b9ce77de721ee9850d464014faba1d14
Xu Cong <xc4330@gmail.com>
2018-02-04 17:13:51 +0800

    Set up test

---
+++ dist/test/ledger.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var _ledger = require('../ledger.js');
+
+var _ledger2 = _interopRequireDefault(_ledger);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+describe('Ledger', function () {
+    describe('parseNames', function () {
+        test('should return list of names seperated by \\n', function () {
+            expect(2 + 2).toBe(4);
+        });
+    });
+});
---
+++ test/ledger.test.js
@@ -0,0 +1,9 @@
+import Ledger from '../ledger.js'
+
+describe('Ledger', () => {
+    describe('parseNames', () => {
+        test('should return list of names seperated by \\n', () => {
+            expect(2 + 2).toBe(4);
+        });
+    })
+})
